ec386382f75998580a334d05d225f471d23a7f5a
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-05-01 20:03:51 -0400

    setting up mongodb and environment files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+require('node:dns/promises').setServers(['1.1.1.1', '8.8.8.8']);
+require('dotenv').config();
 
 const port = 3001;
+
+main()
+  .then(() => console.log('connection successful'))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_CONNECTION_STRING);
+}
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
