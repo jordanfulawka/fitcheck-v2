dc61192fe37af18ffdb048c2278ee20761dd2886
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-05-03 18:46:32 -0400

    feat: style dashboard page

--- client/app/(app)/dashboard/page.tsx
+++ client/app/(app)/dashboard/page.tsx
@@ -1,3 +1,10 @@
+import {
+  Ban,
+  CalendarCheck2,
+  CircleCheckBig,
+  SendHorizonal,
+} from 'lucide-react';
+
 export default async function Dashboard() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/jobs/stats`);
   const { data } = await res.json();
@@ -9,21 +16,45 @@ export default async function Dashboard() {
         <p>Good morning! Ready to apply for some more jobs?</p>
       </div>
       <div className='flex gap-20 px-20 justify-between'>
-        <div className='border border-[#dee2e6] flex-1 bg-white h-64 rounded-lg'>
-          <h1>Total Applications</h1>
-          <p className='text-primary'>{data.result.total}</p>
+        <div className='border border-[#dee2e6] flex-1 bg-white h-32 rounded-lg'>
+          <div className='flex justify-between pt-4 px-4'>
+            <h1 className='text-lg font-bold text-gray-500'>
+              Total Applications
+            </h1>
+            <SendHorizonal size={32} color={'#2346d5'} />
+          </div>
+          <p className='text-primary text-4xl font-bold pl-5'>
+            {data.result.total}
+          </p>
         </div>
         <div className='border border-[#dee2e6] flex-1 bg-white rounded-lg'>
-          <h1>Interviews Scheduled</h1>
-          <p className='text-secondary'>{data.result.interviewing}</p>
+          <div className='flex justify-between pt-4 px-4'>
+            <h1 className='text-lg font-bold text-gray-500'>
+              Interviews Scheduled
+            </h1>
+            <CalendarCheck2 size={32} color={'#8429c8'} />
+          </div>
+          <p className='text-secondary text-4xl font-bold pl-5'>
+            {data.result.interviewing}
+          </p>
         </div>
         <div className='border border-[#dee2e6] flex-1 bg-white rounded-lg'>
-          <h1>Offers Received</h1>
-          <p className='text-[#1a7a4a]'>{data.result.offer}</p>
+          <div className='flex justify-between pt-4 px-4'>
+            <h1 className='text-lg font-bold text-gray-500'>Offers Received</h1>
+            <CircleCheckBig size={32} color={'#1a7a4a'} />
+          </div>
+          <p className='text-[#1a7a4a] text-4xl font-bold pl-5'>
+            {data.result.offer}
+          </p>
         </div>
         <div className='border border-[#dee2e6] flex-1 bg-white rounded-lg'>
-          <h1>Rejections</h1>
-          <p className='text-error'>{data.result.rejected}</p>
+          <div className='flex justify-between pt-4 px-4'>
+            <h1 className='text-lg font-bold text-gray-500'>Rejections</h1>
+            <Ban size={32} color={'#ba1a1a'} />
+          </div>
+          <p className='text-error text-4xl font-bold pl-5'>
+            {data.result.rejected}
+          </p>
         </div>
       </div>
     </div>
